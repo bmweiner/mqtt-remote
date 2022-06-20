150ac8dcfe0c72074709f82feed52ea32d666c00
bmweiner <bmweiner@users.noreply.github.com>
2022-06-20 20:34:52 +0000

    update setup and pyusb

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from evdev import InputDevice, categorize, ecodes
+import os
+os.environ['PYUSB_DEBUG'] = 'debug'
 
-# FLIR = "/dev/input/by-id/usb-flirc.tv_flirc-if01-event-kbd"
-FLIR = "/dev/input/by-id/usb-flirc.tv_flirc-event-if01"
 
-device = InputDevice(FLIR)
-for event in device.read_loop():
-    if event.type == ecodes.EV_KEY:
-        print(categorize(event))
+import usb.core
+import usb.util
+
+
+# flirc device
+dev = usb.core.find(idVendor=0x20a0, idProduct=0x0006)
+
+# was it found?
+if dev is None:
+    raise ValueError('Device not found')
+
+# claim ownership of device
+for cfg in dev:
+  for intf in cfg:
+    print(dev.is_kernel_driver_active(intf.bInterfaceNumber))
+    #   try:
+    #     dev.detach_kernel_driver(intf.bInterfaceNumber)
+
+# set the active configuration. With no arguments, the first
+# configuration will be the active one
+dev.set_configuration()
--- setup.sh
+++ setup.sh
@@ -1,1 +1,21 @@
-# package dependencies
+# apt sources
+sudo cp flirc.list /etc/apt/sources.list.d 
+
+# apt dependencies
+sudo apt update
+sudo apt upgrade
+sudo apt install sense-hat
+sudo apt install flirc
+
+# install pip
+wget https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+rm get-pip.py
+
+# python dependencies
+pip3 install paho-mqtt
+pip3 install pyusb
+
+# install flirc rules
+sudo cp 99-flirc.rules /etc/udev/rules.d/
+#sudo udevadm control --reload-rules && sudo udevadm trigger
